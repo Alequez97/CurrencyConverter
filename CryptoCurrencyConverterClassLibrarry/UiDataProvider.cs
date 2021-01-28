2ecc822faa610e02ae6f46b5ce5ad97d54660088
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-01-28 17:42:40 +0200

    Fix ui data provider class to save loaded data to files

--- CryptoCurrencyConverterClassLibrarry/UiDataProvider.cs
+++ CryptoCurrencyConverterClassLibrarry/UiDataProvider.cs
@@ -12,22 +12,54 @@ namespace CryptoCurrencyConverterClassLibrarry
     {
 
         private IFiatDataLoader fiatDataLoader;
-        private IDataCache dataCache;
+        private IDataCache<FiatCurrencyModel> dataCache;
+
+        private FiatCurrencyModel cachedFiatCurrencyModel = new FiatCurrencyModel() { Date = new DateTime(1970, 1, 1) };  //To check if something was possible to load
 
         public UiDataProvider()
         {
             fiatDataLoader = new FiatDataLoaderFromDataFixerIO();
+            
         }
 
         public FiatCurrencyModel RecieveDataAboutFiatCurreny(FiatCurrencyEnum fiatCurrencyEnum)
         {
-            //check if data is cached
-            // if is, check cached data datetime
-            // if not today try to download data
-            // if success download cache and return new data
-            // if not return last cached data
+            dataCache = new DataToFileCache<FiatCurrencyModel>();
+            cachedFiatCurrencyModel = new FiatCurrencyModel() { Date = new DateTime(1970, 1, 1) }; //To check if something was possible to load
 
-            return fiatDataLoader.LoadDataAboutFiatCurrency(fiatCurrencyEnum).Result;
+            string fileName = fiatCurrencyEnum.ToString() + ".txt";
+
+            if (dataCache.DataIsCached(fileName))
+            {
+                cachedFiatCurrencyModel = dataCache.LoadCachedData(fileName);
+                var currentTicks = DateTime.Now.AddDays(-2).Ticks;
+                if (cachedFiatCurrencyModel.Date.Ticks > currentTicks)
+                {
+                    return cachedFiatCurrencyModel;
+                }
+                else
+                {
+                    return TryLoadDataFromApi(fiatCurrencyEnum);
+                }
+            }
+            else
+            {
+                return TryLoadDataFromApi(fiatCurrencyEnum);
+            }
+        }
+
+        private FiatCurrencyModel TryLoadDataFromApi(FiatCurrencyEnum fiatCurrencyEnum)
+        {
+            try
+            {
+                cachedFiatCurrencyModel = fiatDataLoader.LoadDataAboutFiatCurrency(fiatCurrencyEnum).Result;
+                dataCache.SaveDataToCache(cachedFiatCurrencyModel, fiatCurrencyEnum.ToString() + ".txt");
+                return cachedFiatCurrencyModel;
+            }
+            catch
+            {
+                return cachedFiatCurrencyModel;
+            }
         }
 
     }
